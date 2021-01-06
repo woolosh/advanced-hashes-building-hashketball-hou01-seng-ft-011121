0df3a26e3349e3a1297a6c2f0d8d8920e5fb2bc1
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 05:07:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -92,14 +92,14 @@ def game_hash
         },
         {
           :player_name => "DeSagna Diop",
-          :number => 11,
-          :shoe => 17,
-          :points => 17,
-          :rebounds => 19,
-          :assists => 10,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 15 
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5 
         },    
         {
           :player_name => "Mason Plumlee",
