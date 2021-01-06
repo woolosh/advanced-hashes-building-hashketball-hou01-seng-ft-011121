fe4de096647fc0fde3dc63776b84563ef9bf40c6
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 04:57:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,7 @@ def game_hash
           :player_name => "Alan Anderson",
           :number => 0,
           :shoe => 16,
-          :points
+          :points => 
           
         }
         ]
