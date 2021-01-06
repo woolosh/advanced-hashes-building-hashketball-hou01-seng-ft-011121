e390772ff8b2a61c22f129844b9e3d3a58a7e581
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 05:05:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -69,18 +69,18 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => [
         {
-          :player_name => "Alan Anderson",
-          :number => 0,
-          :shoe => 16,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1 
+          :player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2 
         },
         {
-          :player_name => "Reggie Evans",
+          :player_name => "Bismack Biyombo",
           :number => 30,
           :shoe => 14,
           :points => 12,
