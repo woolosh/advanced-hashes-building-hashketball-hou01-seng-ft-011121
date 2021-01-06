4ffdf87f539c1d517bed3a41b2075751fe8b61d2
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 04:56:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,15 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{}]
+      :players => [
+        {
+          :player_name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points
+          
+        }
+        ]
     },
     :away => {}
     
