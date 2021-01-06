a77cea423395225231b26f90dfd5100b0b5302e8
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 04:54:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,13 @@ require 'pry'
 def game_hash
   
   hashketball = {
-    {:home
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [{}]
+    },
+    :away => {}
+    
   }
   
   
